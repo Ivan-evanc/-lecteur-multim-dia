d1e6a6000ebc4fd63b78268d0c58615eb11fefe7
evans <evanskibet114@gmail.com>
2018-01-04 08:02:38 +0300

    first commit

---
+++ Media Player app/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace Media_Player_app
+{
+
+    public partial class MainWindow : Window
+    {
+        string selectFile;
+        private SoundPlayerAction soundPlayer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.AddExtension = true;
+            fileDialog.DefaultExt = "";
+            fileDialog.Title = "Find the file to choose";
+            fileDialog.Filter="media(*.*)|*.*";
+            selectFile = fileDialog.FileName;
+            fileDialog.ShowDialog();
+
+            mediaElement.Source = new Uri(fileDialog.FileName, UriKind.RelativeOrAbsolute);
+           
+           // textBox.Text = fileDialog.SafeFileName;
+            textBox1.Text = fileDialog.SafeFileName;
+
+
+
+
+        }
+
+        private void mediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Play();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
+        }
+    }
+}
